34387bed394e5f3a1af589b7d9cace6b6819e2c4
David Sackett <david@davidsackett.com>
2013-08-26 22:27:36 +1000

    Completed parsing of boxes

--- parser.py
+++ parser.py
@@ -7,16 +7,18 @@ Parses the elements of an ascii art state machine diagram
 
 class Point:
     '''represents a point using x and y coordinates'''
-    def __init__(self, x, y):
+    def __init__(self, x, y, value):
         self.x = x
         self.y = y
+        self.value = value
 
     def __eq__(self, other):
         return (self.x == other.x and 
-                self.y == other.y)
+                self.y == other.y and
+                self.value == other.value)
 
     def __repr__(self):
-        return 'Point({}, {}'.format(self.x, self.y)
+        return 'Point({}, {}, {})'.format(self.x, self.y, self.value)
 
 class Box:
     '''represents a box using two points'''
@@ -27,31 +29,68 @@ class Box:
     @classmethod
     def parse(cls, diagram):
         '''returns all the Boxes in the given diagram'''
+        # TODO parse more efficiently. Could follow edge from first '+'
 
         # search for '+' and determine if top left or botton right
-        # +- or  |
-        # |     -+
+        candidates = [p for p in diagram if p.value == '+']
 
-        candidates = [(x, y, p) for x, y, p in diagram if p == '+']
-
-        top_right_candidates = [
-            (x, y, p) for x, y, p in candidates
-                if (diagram[x+1, y] == '-' and 
-                    diagram[x, y+1] == '|')]
+        # check if '+' is part of a top left corner:
+        # +-
+        # | 
+        top_left_candidates = [
+            p for p in candidates
+                if (diagram[p.x+1, p.y].value == '-' and 
+                    diagram[p.x, p.y+1].value == '|')]
                     
+
+        # check if '+' is part of a bottom right corner:
+        #  |
+        # -+ 
         bottom_right_candidates = [
-            (x, y, p) for x, y, p in candidates
-                if (diagram[x-1, y] == '-' and 
-                    diagram[x, y-1] == '|')]
+            p for p in candidates
+                if (diagram[p.x-1, p.y].value == '-' and 
+                    diagram[p.x, p.y-1].value == '|')]
+
+        #print(candidates)
+        #print(top_left_candidates)
+        #print(bottom_right_candidates)
 
         # try all combination of top right and bottom left corners to see if
         # the sides are unbroken
-        print(candidates)
-        print(top_right_candidates)
-        print(bottom_right_candidates)
+        boxes = []
+        for top_left_candidate in top_left_candidates:
+            for bottom_right_candidate in bottom_right_candidates:
+                if cls.closed_box(top_left_candidate, 
+                                  bottom_right_candidate, diagram):
+                    boxes.append(Box(top_left_candidate, 
+                                     bottom_right_candidate))
 
+        return boxes
 
-        #return [cls(Point(1,5), Point(4,9))]
+    @classmethod
+    def closed_box(cls, top_left, bottom_right, diagram):
+        '''returns true if the two points form the corners of a closed box'''
+        # check top edge
+        for p in diagram[top_left.x+1:bottom_right.x, top_left.y]:
+            if p.value != '-':
+                return False
+
+        # check bottom edge
+        for p in diagram[top_left.x+1:bottom_right.x, bottom_right.y]:
+            if p.value != '-':
+                return False
+
+        # check left edge
+        for p in diagram[top_left.x, top_left.y+1:bottom_right.y]:
+            if p.value != '|':
+                return False
+
+        # check right edge
+        for p in diagram[bottom_right.x, top_left.y+1:bottom_right.y]:
+            if p.value != '|':
+                return False
+
+        return True
 
     def __eq__(self, other):
         return (self.top_left == other.top_left and 
@@ -74,14 +113,30 @@ class Diagram:
         # TODO __iter__ should use __getitem__
         for y, row in enumerate(self.canvas):
             for x, col in enumerate(row):
-                yield x, y, col
+                yield self[x, y]
 
     def __getitem__(self, val):
-        x, y = val
-        # lots of tests are done against adjacent points so we return None
-        # rather than IndexError to avoid lots of catching the exception
-        # TODO how to gracefully handle IndexErrors? Just return None?
+        x_slice, y_slice = val
         try:
-            return self.canvas[y][x]
+            # return a single point if indexed with ints
+            if (type(x_slice) == int and
+                type(y_slice) == int):
+                return Point(x_slice, y_slice, self.canvas[y_slice][x_slice])
+
+            ps = []
+            if type(x_slice) == int:
+                x_slice = slice(x_slice, x_slice+1)
+            if type(y_slice) == int:
+                y_slice = slice(y_slice, y_slice+1)
+
+            for y in range(len(self.canvas))[y_slice]:
+                for x in range(len(self.canvas[y]))[x_slice]:
+                    #print('x:{}, y:{}, canvas:{}'.format(x, y, self.canvas[y][x]))
+                    ps.append(Point(x, y, self.canvas[y][x]))
+            return ps
         except IndexError:
-            return None
+            # lots of tests are done against adjacent points so we return None
+            # rather than IndexError to avoid lots of catching the exception
+            # TODO how to gracefully handle IndexErrors? Just return None?
+            return Point(None, None, None)
+
--- test_parser.py
+++ test_parser.py
@@ -26,23 +26,36 @@ cd'''
     def test_diag_iter(self):
         d1 = Diagram('''ab
 cd''')
-        assert(list(d1) == [(0, 0, 'a'), (1, 0, 'b'), (0, 1, 'c'), (1, 1, 'd')]) 
+        assert(list(d1) == [Point(0, 0, 'a'), Point(1, 0, 'b'), 
+                            Point(0, 1, 'c'), Point(1, 1, 'd')]) 
 
         # test that indexing matches iter
-        for x, y, p in d1:
-            print(x, y, p, d1[x, y])
-            assert(p == d1[x, y])
-        
+        for p in d1:
+            assert(p == d1[p.x, p.y])
+
+    def test_diag_slice(self):
+        d = Diagram('''abc
+def
+ghi''')
+        assert(list(d[:,0]) == [Point(0, 0, 'a'), Point(1, 0, 'b'), 
+                                Point(2, 0, 'c')])
+        assert(list(d[1:2,0]) == [Point(1, 0, 'b')])
+        assert(list(d[0,:]) == [Point(0, 0, 'a'), Point(0, 1, 'd'), 
+                                Point(0, 2, 'g')])
+        assert(list(d[0,1:2]) == [Point(0, 1, 'd')])
 
 def test_points_eq():
-    assert(Point(1,1) == Point(1,1))
-    assert(Point(1,1) != Point(1,2))
+    assert(Point(1, 1, 'a') == Point(1, 1, 'a'))
+    assert(Point(1, 1, 'a') != Point(1, 2, 'a'))
 
 def test_boxes_eq():
-    assert(Box(Point(1,1), Point(2,2)) == Box(Point(1,1), Point(2,2)))
-    assert(Box(Point(1,1), Point(2,3)) != Box(Point(1,1), Point(2,2)))
+    assert(Box(Point(1,1, 'a'), Point(2,2, 'a'))
+           == Box(Point(1,1, 'a'), Point(2,2, 'a')))
+    assert(Box(Point(1,1, 'a'), Point(2,3, 'a')) 
+           != Box(Point(1,1, 'a'), Point(2,2, 'a')))
 
 def test_parse_boxes():
+    # complete box
     box = '''
     +---+
     |   |
@@ -51,7 +64,41 @@ def test_parse_boxes():
 
 '''
     diag = Diagram(box)
-    print(diag)
-    #boxes = diag.box
     boxes = Box.parse(diag)
-    assert(boxes == [Box(Point(1,5), Point(4, 9))])
+    assert(boxes == [Box(Point(4,1, '+'), Point(8, 4, '+'))])
+
+    # broken box
+    box = '''
+    +- -+
+    |   |
+    |   |
+    +---+
+
+'''
+    diag = Diagram(box)
+    assert([] == Box.parse(diag))
+
+    box = '''
+    +---+
+    |    
+    |   |
+    +---+
+
+'''
+    diag = Diagram(box)
+    assert([] == Box.parse(diag))
+
+def xtest_parses_diagram_with_uneven_rows():
+    # TODO parse diagram with uneven rows see below. The second row of box is
+    # one character shorter than the other rows
+    # broken box
+    box = '''
+    +---+
+    |   
+    |   |
+    +---+
+
+'''
+    diag = Diagram(box)
+    assert([] == Box.parse(diag))
+
